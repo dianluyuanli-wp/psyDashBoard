0945d9413563591f3c3f94f5899d210dea984087
xiaohuli <waterdoor@foxmail.com>
2020-04-28 11:05:41 +0800

    fix: rawString

--- src/pages/PageManager/index.tsx
+++ src/pages/PageManager/index.tsx
@@ -1,15 +1,14 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React, { useState, useEffect, useRef } from 'react';
-import { Upload, message, Button, Modal, Input, Form } from 'antd';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
+import { Upload, message, Button, Modal, Form } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { upload, getBase64 } from '@/utils/upload';
 import { uploadPageInfo, getPageInfo } from '@/services/pageManager';
-import {UploadFile, UploadChangeParam } from 'antd/lib/upload/interface';
+import { UploadFile, UploadChangeParam } from 'antd/lib/upload/interface';
 import BraftEditor, { EditorState } from 'braft-editor';
 import styles from './index.less';
-import 'braft-editor/dist/index.css'
+import 'braft-editor/dist/index.css';
 
-const { TextArea } = Input;
 const { Item: FormItem } = Form;
 
 const layout = {
@@ -17,8 +16,14 @@ const layout = {
   wrapperCol: { span: 21 },
 };
 
-interface FormData {
-  user: { pageContent: string }
+// interface FormData {
+//   user: { pageContent: string }
+// }
+
+interface EditorObj {
+  state: EditorState;
+  text: string;
+  rawString: string;
 }
 
 const UploadImgWall = () => {
@@ -29,8 +34,16 @@ const UploadImgWall = () => {
   const [previewVisible, setPreviewStatus] = useState(false);
   //  预览的图片
   const [previewImg, setPreviewImg] = useState('');
+
   //  富文本编辑器内容
-  const [editorState, setEditorState] = useState(BraftEditor.createEditorState(null));
+  function editorReducer(state: EditorObj, action: EditorObj): EditorObj {
+    return { ...state, ...action };
+  }
+  const [editorState, setEditorState] = useReducer(editorReducer, {
+    state: BraftEditor.createEditorState(null),
+    text: '',
+    rawString: '',
+  });
 
   const formRef = useRef(null);
 
@@ -38,15 +51,15 @@ const UploadImgWall = () => {
   useEffect(() => {
     async function pageInfo() {
       const { data } = await getPageInfo();
-      const { imgList, text } = JSON.parse(data[0]);
+      const { imgList, text, rawString } = JSON.parse(data[0]);
       setFileList(imgList);
-      formRef.current.setFieldsValue({
-        user: {
-          pageContent: text
-        }
-      })
-      setEditorState(BraftEditor.createEditorState(text));
-    };
+      setEditorState({
+        //  rawString必须是双引号的JSON字符串格式
+        state: BraftEditor.createEditorState(rawString.replace(/'/g, '"')),
+        rawString,
+        text,
+      });
+    }
     pageInfo();
   }, []);
 
@@ -56,8 +69,8 @@ const UploadImgWall = () => {
         return;
       }
       Object.assign(file, {
-        preview: await getBase64(file.originFileObj)
-      })
+        preview: await getBase64(file.originFileObj),
+      });
     }
     setPreviewImg(file.url || file.preview || '');
     setPreviewStatus(true);
@@ -79,14 +92,15 @@ const UploadImgWall = () => {
       message.error(`${info.file.name} 上传失败。`);
       return;
     }
-    setFileList(newFileList.map(item => {
-      if (item.name === file.name) {
-        return Object.assign(item, { status: 'done', url: ans.file_list[0].download_url });
-      }
-      return item;
-    }));
+    setFileList(
+      newFileList.map(item => {
+        if (item.name === file.name) {
+          return Object.assign(item, { status: 'done', url: ans.file_list[0].download_url });
+        }
+        return item;
+      }),
+    );
   }
-
 
   const uploadButton = (
     <div>
@@ -94,64 +108,87 @@ const UploadImgWall = () => {
       <div className="ant-upload-text">Upload</div>
     </div>
   );
-  async function updatePageInfo(value: Object) {
-    const pageInfo = value as FormData;
-    const clearImgList = fileList.map(({ uid, size, name, type, url = '' }) => ({ uid, size, name, type, url }));
+  async function updatePageInfo() {
+    const clearImgList = fileList.map(({ uid, size, name, type, url = '' }) => ({
+      uid,
+      size,
+      name,
+      type,
+      url,
+    }));
     const res = await uploadPageInfo({
       imgList: clearImgList,
-      //  text: pageInfo.user.pageContent.toString()
-      text: editorState.toHTML().replace(/"/g, "'")
-    })
+      text: editorState.text,
+      rawString: editorState.rawString,
+    });
     if (res.errmsg === 'ok') {
       message.success('更新成功');
     } else {
-      message.error('更新失败')
+      message.error('更新失败');
     }
   }
 
   function handelEditor(currentEditorState: EditorState) {
-    setEditorState(currentEditorState);
+    setEditorState({
+      state: currentEditorState,
+      //  上传的时候必须是单引号，否则云开发后台存储会报错
+      text: currentEditorState.toHTML().replace(/"/g, "'"),
+      rawString: currentEditorState.toRAW().replace(/"/g, "'"),
+    });
+  }
+
+  async function inlineImgUpload(para: any) {
+    const res = await upload({
+      file: {
+        originFileObj: para.file,
+      },
+    } as UploadChangeParam<UploadFile<any>>);
+    const url = res.file_list[0].download_url;
+    para.success({
+      url,
+    });
+    console.log(url);
   }
 
   return (
     <div>
-        <Modal visible={previewVisible} footer={null} onCancel={handleCancel}>
-          <img alt="example" style={{ width: '100%' }} src={previewImg} />
-        </Modal>
-        <Form {...layout} ref={formRef} onFinish={value => updatePageInfo(value)}>
-          <FormItem label='首页轮播图'>
-            <Upload
-              listType="picture-card"
-              fileList={fileList}
-              onPreview={handlePreview}
-              showUploadList={{showDownloadIcon: false}}
-              onChange={handleChange}
-              //  不要跑默认的智障上传方法，每上传一次就搞个post请求，有的时候还会卡状态
-              customRequest={() => {}}
-            >
-              {fileList.length >= 3 ? null : uploadButton}
-            </Upload>
-          </FormItem>
-          <FormItem label='内容' name={['user', 'pageContent']}>
-            <TextArea rows={4}/>
-          </FormItem>
-          <BraftEditor
-            value={editorState}
-            onChange={handelEditor}
-            media={{
-
-            }}
-          />
-          <Button className={styles.btn} type='primary' htmlType="submit">页面信息上传</Button>
-        </Form>
+      <Modal visible={previewVisible} footer={null} onCancel={handleCancel}>
+        <img alt="example" style={{ width: '100%' }} src={previewImg} />
+      </Modal>
+      <Form {...layout} ref={formRef} onFinish={updatePageInfo}>
+        <FormItem label="首页轮播图">
+          <Upload
+            listType="picture-card"
+            fileList={fileList}
+            onPreview={handlePreview}
+            showUploadList={{ showDownloadIcon: false }}
+            onChange={handleChange}
+            //  不要跑默认的智障上传方法，每上传一次就搞个post请求，有的时候还会卡状态
+            customRequest={() => {}}
+          >
+            {fileList.length >= 3 ? null : uploadButton}
+          </Upload>
+        </FormItem>
+        <BraftEditor
+          value={editorState.state}
+          onChange={handelEditor}
+          media={{
+            uploadFn: inlineImgUpload,
+          }}
+        />
+        <Button className={styles.btn} type="primary" htmlType="submit">
+          页面信息上传
+        </Button>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default () => 
-    <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
-      {/* <div style={{ paddingTop: 100, textAlign: 'center' }}>
+export default () => (
+  <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
+    {/* <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Spin spinning={loading} size="large"></Spin>
       </div> */}
-        <UploadImgWall />
-    </PageHeaderWrapper>;
+    <UploadImgWall />
+  </PageHeaderWrapper>
+);
--- src/services/pageManager.ts
+++ src/services/pageManager.ts
@@ -1,24 +1,25 @@
 import { reqPost } from './commonUtils';
 
 interface singleImg {
-    uid: string, 
-    size: number, 
-    name: string, 
-    type: string, 
-    url: string
+  uid: string;
+  size: number;
+  name: string;
+  type: string;
+  url: string;
 }
 
 interface UpdatePara {
-    imgList: Array<singleImg>;
-    text: string
+  imgList: Array<singleImg>;
+  text: string;
+  rawString: string;
 }
-  
+
 //  更新页面状态
 export async function uploadPageInfo(params: UpdatePara) {
-    return reqPost('uploadPageInfo', params);
+  return reqPost('uploadPageInfo', params);
 }
 
 //  拉取页面状态
 export async function getPageInfo() {
-    return reqPost('getPageInfo', {});
+  return reqPost('getPageInfo', {});
 }
