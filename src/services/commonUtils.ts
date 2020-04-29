481615c98b0e1ab168bf3f8ca3853b249a6df03b
xiaohuli <waterdoor@foxmail.com>
2020-04-29 11:22:13 +0800

    fix: 自动切换api域名

--- src/services/commonUtils.ts
+++ src/services/commonUtils.ts
@@ -1,15 +1,13 @@
 import request from '@/utils/request';
+import { isAntDesignProOrDev } from '@/utils/utils';
 
-export const domain = 'http://localhost:4000/api/';
+export const domain = isAntDesignProOrDev()
+  ? 'http://localhost:4000/api/'
+  : 'http://47.75.180.129/api/';
 
 //  export const domain = 'http://47.75.180.129/api/';
 
-// export const commonPara = {
-//   name: localStorage.getItem('name'),
-//   token: localStorage.getItem('tk'),
-// };
-
-export const reqPost = (url: string, para: object) => 
+export const reqPost = (url: string, para: object) =>
   request(domain + url, {
     method: 'post',
     data: Object.assign({}, para, {
@@ -18,8 +16,8 @@ export const reqPost = (url: string, para: object) =>
     }),
   });
 
-export const purePost = (url: string, para: object) => 
+export const purePost = (url: string, para: object) =>
   request(domain + url, {
     method: 'post',
-    data: para
-});
+    data: para,
+  });
